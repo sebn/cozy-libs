d88ed53583a0dfe121ca83f0f0f19520eeb21531
Yannick Lohse <contact@yannick-lohse.fr>
2019-06-10 16:11:07 +0200

    fix: Moved Nav component to individual playgrounds

--- packages/playgrounds/src/common/App.jsx
+++ packages/playgrounds/src/common/App.jsx
@@ -12,7 +12,6 @@ import { I18n } from 'cozy-ui/transpiled/react'
 import { getUniversalLinkDomain } from 'cozy-ui/transpiled/react/AppLinker'
 import icon from './icon.png'
 import { MobileRouter } from 'cozy-authentication'
-import Nav from './Nav'
 
 const styles = {
   error: {
@@ -61,7 +60,6 @@ class App extends React.Component {
     const { title, icon, client, children, existingStore } = this.props
     return (
       <ErrorBoundary>
-        <Nav />
         <I18n dictRequire={() => ({})} lang="en">
           <CozyProvider store={existingStore} client={client}>
             <MobileRouter
